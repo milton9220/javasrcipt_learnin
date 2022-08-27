be2e6d3ff5b351ccd41e0d2199c29da46d387318
milton9220 <monjurhasan105142@gmail.com>
2022-08-28 00:50:47 +0600

    first file added

---
+++ app.js
@@ -0,0 +1,43 @@
+
+/*
+var num1=160;
+var num2=60;
+var num3=120;
+
+//find maximum number
+
+if(num1 > num2 && num1 > num3){
+    console.log(num1+" is bigger");
+}
+else if(num2 > num1 && num2 > num3){
+    console.log(num2+" is bigger");
+}
+else{
+    console.log(num3+" is bigger");
+}
+
+*/
+
+/*var numbers=[100,200,120,800,60,200];
+
+//find array maximum number
+var max=numbers[0];
+
+for(var i=0;i<numbers.length;i++){
+    if(max < numbers[i]){
+        max=numbers[i];
+        var index=numbers.indexOf(max);
+    }
+    
+}
+console.log(max);
+console.log(index);
+*/
+var numbers=[1,3,4,5,6];
+var sum=0;
+for(var i=0;i<numbers.length;i++){
+    sum+=numbers[i];
+}
+console.log(sum);
+
+
